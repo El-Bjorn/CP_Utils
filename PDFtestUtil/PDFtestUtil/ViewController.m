83b71caa2fc0a37f69582a27959e3fc1e0237f3f
Bjorn Chambless <bjorn@builtlight.org>
2014-10-29 16:17:35 -0700

    Initial commit of PDFtestUtil: it lets you take a picture and email it off as a pdf attachment.

---
+++ PDFtestUtil/PDFtestUtil/ViewController.m
@@ -0,0 +1,79 @@
+//
+//  ViewController.m
+//  PDFtestUtil
+//
+//  Created by BjornC on 10/29/14.
+//  Copyright (c) 2014 Builtlight. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MEssageUI/MFMailComposeViewController.h>
+
+
+@interface ViewController () <UIImagePickerControllerDelegate,
+                                UINavigationControllerDelegate,
+                                MFMailComposeViewControllerDelegate>
+
+//@property (weak, nonatomic) IBOutlet UIWebView *webview;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+-(IBAction) takePictureOfBill:(id)sender {
+    NSLog(@"You takee pwetty pictia now.");
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    picker.cameraDevice = UIImagePickerControllerCameraDeviceRear;
+    picker.showsCameraControls = YES;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+    
+}
+
+- (void) imagePickerController:(UIImagePickerController *)picker
+                    didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    self.imageView.image = image;
+    [self sendPDF:image];
+}
+
+-(void) sendPDF:(UIImage*)img {
+    
+    NSString *path;
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"CoPatientTstPDF"];
+    
+    UIGraphicsBeginPDFContextToFile(path, CGRectZero, nil);
+    UIGraphicsBeginPDFPageWithInfo(CGRectZero, nil);
+    
+    [img drawAtPoint:CGPointZero];
+    UIGraphicsEndPDFContext();
+    
+    // send mail
+    MFMailComposeViewController *mailVC = [[MFMailComposeViewController alloc] init];
+    mailVC.mailComposeDelegate = self;
+    [mailVC setSubject:@"CP PDF test"];
+    [mailVC addAttachmentData:[NSData dataWithContentsOfFile:path]
+                     mimeType:@"application/pdf" fileName:@"tstPDF"];
+    [self presentViewController:mailVC animated:YES completion:nil];
+
+    
+}
+
+-(void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+
+
+-(void) viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+@end
