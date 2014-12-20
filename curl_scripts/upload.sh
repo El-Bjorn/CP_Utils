f1ac33fdcbd4caaf3b7054ba0214012e866a58ad
Bjorn Chambless <bjorn@builtlight.org>
2014-12-19 21:57:50 -0800

    upload.sh revisions suggested by Alex.

--- curl_scripts/upload.sh
+++ curl_scripts/upload.sh
@@ -18,7 +18,8 @@ encoded_pdf_file="/tmp/CP_tmp_file"
 echo "auth_token: $auth_token"
 
 echo "base64 encoding pdf file..."
-cat $orig_pdf_file | uuencode -m "base64XXencode" | tr -d '\n' | sed 's/begin-base64 644 base64XXencode//' | sed 's/=====//' > $encoded_pdf_file
+cat $orig_pdf_file | uuencode -m "base64XXencode" | sed 's/=/%3D/g' | tail -n 2 | perl -p -e 's/\n/%2B/' > $encoded_pdf_file
+#exit
 
 # 'openssl' must be 1.0.1+, or some version that does SHA256
 #	  note: the mac one is 9.8 and doesn't do SHA256
@@ -33,21 +34,23 @@ mime_type="application%2Fpdf"
 
 upload_request=$upload_URI'?access_key='$access_key'&token='$auth_token'&filename='$filename'&mime_type='$mime_type'&contents='$(cat $encoded_pdf_file)'&story='$description
 
-echo "upload_request = $upload_request"
+#echo "upload_request = $upload_request"
+#exit
 
 raw_hash_value=$(echo -n $upload_request | $openssl sha256 -hmac $secret_key)
 #echo "raw openssl output:  $raw_hash_value"
 
 #  strip off the pointless '(stdin)=' field that some openssls return
 hash_value=$(echo -n $raw_hash_value | awk '{print $2}') 
-echo "hash_value= $hash_value"
+#echo "hash_value= $hash_value"
 #exit
 
 post_param='access_key='$access_key'&token='$auth_token'&filename='$filename'&mime_type='$mime_type'&contents='$(cat $encoded_pdf_file)'&story='$description'&signature='$hash_value
-#echo "post_param= $post_param"
+echo "post_param= $post_param"
+#exit
 
 full_URI=$CP_URL$list_URI
 #echo "full_URI= $full_URI"
 
 # take out or add '-v' depending on whether you want the details
-/usr/bin/curl -v --data $post_param $full_URI 
+/usr/bin/curl --data $post_param $full_URI 
